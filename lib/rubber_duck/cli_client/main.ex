bd54d8198d6148a13326c98244ae4eebe94f484e
Pascal Charbonneau <pcharbon70@gmail.com>
2025-07-14 12:05:34 -0400

    Fix unused alias Client warning in main.ex
    - Remove unused Client from the alias list
    - Keep only Auth and UnifiedIntegration which are actually used
    
    This eliminates the "unused alias Client" warning.

--- lib/rubber_duck/cli_client/main.ex
+++ lib/rubber_duck/cli_client/main.ex
@@ -5,7 +5,7 @@ defmodule RubberDuck.CLIClient.Main do
   This module handles command-line parsing and routing to appropriate handlers.
   """
 
-  alias RubberDuck.CLIClient.{Auth, Client, UnifiedIntegration}
+  alias RubberDuck.CLIClient.{Auth, UnifiedIntegration}
   require Logger
 
   @app_name "rubber_duck"
